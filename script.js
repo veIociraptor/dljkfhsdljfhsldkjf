de969492eb0b49af56a79de5e7d16d407cb41ef2
veIociraptor <40887537+veIociraptor@users.noreply.github.com>
2020-01-21 16:46:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let cls = function (id) {
     return document.getElementsByClassName(id);
 };
 
+let user  = {
+    name: $('nm'),
+    age: $('dt'),
+    wt:  $('wt'),
+    ht:  $('ht'),
+    imt: 0,
+};
+function test() {
+    user.imt = Number(Math.floor((this.wt.value / Math.pow(this.ht.value, 2) * 10000)));
+    alert(user.imt);
+};
+
+
+
 window.onload = function() {
     let clrTxt = cls("tblLi");
     let day = $("day");
@@ -15,42 +29,25 @@ window.onload = function() {
     switch(date.getDay()) {
         case 1:
         case 3:
-        case 6: day.style.backgroundColor = "violet";
+        case 6: day.style.backgroundColor = '#7f4494';
         for (let i = 0; i < clrTxt.length; i++) {
-            clrTxt[i].style.color = "violet";
+            clrTxt[i].style.color = '#7f4494';
         }
         break;
         case 2:
-        case 5: day.style.backgroundColor = "aqua";
+        case 5: day.style.backgroundColor = "#35c1d2";
             for (let i = 0; i < clrTxt.length; i++) {
-                clrTxt[i].style.color = "aqua";
+                clrTxt[i].style.color = "#35c1d2";
             }
         break;
         case 4:
-        case 0: day.style.backgroundColor = "orange";
+        case 0: day.style.backgroundColor = "#f26722";
             for (let i = 0; i < clrTxt.length; i++) {
-                clrTxt[i].style.color = "orange";
+                clrTxt[i].style.color = "#f26722";
             }
         break;
     }
 
-
-    let user  = {
-        name: $('nm').value,
-        age: $('dt').value,
-        wt:  $('wt').value,
-        ht:  $('ht').value,
-        imt: Math.floor((this.wt / Math.pow(this.ht, 2) * 10000)),
-    };
-
-
-    $('calcB').onclick = function () {
-        alert("ку");
-    }
-
-
 };
-
-
 
 
